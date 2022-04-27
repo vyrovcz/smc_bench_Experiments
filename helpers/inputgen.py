93890138375e9076d1d29ca66942a92b28da7f5c
DESKTOP-2FJQOQP\jobrm <j.obrman@tum.de>
2022-04-27 15:03:04 +0200

    debug test, added help funcs

--- helpers/inputgen.py
+++ helpers/inputgen.py
@@ -16,7 +16,8 @@ try:
         n = int(sys.argv[1])
         seed(int(sys.argv[2]))
 except:
-        print("inputgen.py: usage: ./inputgen.py <amount> <seed>")
+        print("Usage: ./inputgen.py <amount> <seed>")
+        print("Print <amount> many random numbers in range [1:3*<amount>]")
         exit()
 
 print(set_to_string(get_random_set(n)))
