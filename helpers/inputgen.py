1fb5fd32b965fa286fb38bf68e81698c6bd4d365
DESKTOP-2FJQOQP\jobrm <j.obrman@tum.de>
2022-05-30 16:48:41 +0200

    tupel and float support & cleanup

--- helpers/inputgen.py
+++ helpers/inputgen.py
@@ -9,15 +9,26 @@ def get_random_set(n: int):
                 rand_set.add(randint(1, 3*n))
         return rand_set
 
+def get_random_tupel(n: int):
+        return [randint(1, 3*n) for i in range(n)]
+
 def set_to_string(s: set):
         return ' '.join(map(str, s))
 
 try:
-        n = int(sys.argv[1])
-        seed(int(sys.argv[2]))
+        option = sys.argv[1][-1]
+        n = int(sys.argv[2])
+        seed(int(sys.argv[3]))
+
 except:
-        print("Usage: ./inputgen.py <amount> <seed>")
-        print("Print <amount> many random numbers in range [1:3*<amount>]")
+        print("Usage: ./inputgen.py <option> <amount> <seed>")
+        print("Options:")
+        print("  -s: set,   Print <amount> many unique random numbers in range [1:3*<amount>]")
+        print("  -t: tupel, Print <amount> many random numbers in range [1:3*<amount>]")
         exit()
 
-print(set_to_string(get_random_set(n)))
+match option:
+        case 't':
+                print(set_to_string(get_random_tupel(n)))
+        case _:
+                print(set_to_string(get_random_set(n)))
