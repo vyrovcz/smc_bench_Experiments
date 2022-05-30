a4cd71ea780aa65bdacdb46ac3ddcd05d5a98af8
DESKTOP-2FJQOQP\jobrm <j.obrman@tum.de>
2022-05-30 17:45:33 +0200

    python 3.9 and lower supported variant

--- helpers/inputgen.py
+++ helpers/inputgen.py
@@ -1,16 +1,22 @@
 #! /usr/bin/python
 
-from random import randint, seed
+from random import randint, seed, random
 import sys
+
+rand_range = 3
+float_precision = 5
 
 def get_random_set(n: int):
         rand_set = set()
         while len(rand_set) < n:
-                rand_set.add(randint(1, 3*n))
+                rand_set.add(randint(1, rand_range*n))
         return rand_set
 
 def get_random_tupel(n: int):
-        return [randint(1, 3*n) for i in range(n)]
+        return [randint(1, rand_range*n) for i in range(n)]
+        
+def get_random_float_tupel(n: int):
+        return [randint(1, rand_range*n) + round(random(), float_precision) for i in range(n)]
 
 def set_to_string(s: set):
         return ' '.join(map(str, s))
@@ -25,10 +31,22 @@ except:
         print("Options:")
         print("  -s: set,   Print <amount> many unique random numbers in range [1:3*<amount>]")
         print("  -t: tupel, Print <amount> many random numbers in range [1:3*<amount>]")
+        print("  -f: float, Print <amount> many random numbers in range [1:3*<amount>]")
         exit()
 
-match option:
-        case 't':
-                print(set_to_string(get_random_tupel(n)))
-        case _:
-                print(set_to_string(get_random_set(n)))
+# python3.9 and lower support
+if (option == 't'):
+        print(set_to_string(get_random_tupel(n)))
+elif (option == 'f'):
+        print(set_to_string(get_random_float_tupel(n)))
+else:
+        print(set_to_string(get_random_set(n)))
+
+#for future python3.10 and higher releases
+#match option:
+#        case 't':
+#                print(set_to_string(get_random_tupel(n)))
+#        case 'f':
+#                print(set_to_string(get_random_float_tupel(n)))
+#        case _:
+#                print(set_to_string(get_random_set(n)))
